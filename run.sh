52a4c656e6c23c98f54831a98f18753902d380ca
Ari Archer <ari@ari.lt>
2024-06-07 19:04:37 +0300

    source ari-lt.env pre-cd
    Signed-off-by: Ari Archer <ari@ari.lt>

--- run.sh
+++ run.sh
@@ -6,10 +6,9 @@ main() {
     kill -9 $(pgrep python3) || true
     kill -9 $(pgrep gunicorn) || true
 
+    source ari-lt.env
     cd src
     python3 -m pip install gunicorn
-
-    source ari-lt.env
     python3 -m gunicorn -b 127.0.0.1:17312 -w 1 main:app &
 }
 
